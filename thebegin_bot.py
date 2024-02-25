c45a150e4cdb225e504f28d87e46c0bb4ce465c1
trekstorgg <amazonprimebd2@gmail.com>
2024-02-25 18:14:59 +0100

    Update Procfile for web

--- thebegin_bot.py
+++ thebegin_bot.py
@@ -1,19 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from telegram import Update
 from telegram.ext import PicklePersistence, Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from fuzzywuzzy import fuzz
+import logging
+
+logging.basicConfig(
+    level=logging.ERROR,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
 
 TOKEN = '6819081129:AAGpDpi1c4UP4Duis0JGsK2vamaEcLg0b5Q'
 
-def start(update, context):
-    update.message.reply_text('Привет! Я ваш бот.')
+def start(update: Update, context: CallbackContext):
+    context.bot.send_message(update.message.chat_id, 'Привет, малюск')
 
-def echo(update, context):
-    update.message.reply_text(update.message.text)
+def help_command(update: Update, context: CallbackContext):
+    context.bot.send_message(update.message.chat_id, 'Это ваш бот. Доступные команды:\n/start - начать диалог\n/help - получить справку')
+
+def how_are_you(update: Update, context: CallbackContext):
+    user_message = update.message.text.lower()
+    expected_phrases = ["как дела?", "как дила?"]
+    best_match, score = max(((phrase, fuzz.ratio(user_message, phrase)) for phrase in expected_phrases), key=lambda x: x[1])
+
+    if score >= 80:
+        context.bot.send_message(update.message.chat_id, f'Спасибо, что спросил! У меня всё отлично. А у тебя как дела?')
+        logging.info(f"Received '{best_match}' message")
+    else:
+        context.bot.send_message(update.message.chat_id, 'Извините, не понял вас. Можете переформулировать вопрос?')
+
+def what_are_you_doing(update: Update, context: CallbackContext):
+    context.bot.send_message(update.message.chat_id, 'Я занят общением с вами! Чем могу помочь?')
 
 def main():
-    persistence = PicklePersistence('conversation_data.pkl')
+    persistence = PicklePersistence('/app/conversation_data.pkl')
     updater = Updater(TOKEN, use_context=True, persistence=persistence)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dp.add_handler(CommandHandler("help", help_command))
+
+    dp.add_handler(MessageHandler(Filters.regex(r'как дела\??') | Filters.regex(r'как дила\??'), how_are_you))
+    dp.add_handler(MessageHandler(Filters.regex(r'что ты делаешь\??'), what_are_you_doing))
+
     updater.start_polling()
     updater.idle()
 
