7304c25b31b846ccdbd2b2aa3d5fd9df61d87a2d
trekstorgg <amazonprimebd2@gmail.com>
2024-02-25 19:41:37 +0100

    Описание изменений

--- thebegin_bot.py
+++ thebegin_bot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
 from telegram import Update
 from telegram.ext import PicklePersistence, Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from fuzzywuzzy import fuzz
@@ -35,4 +36,23 @@ def what_are_you_doing(update: Update, context: CallbackContext):
     context.bot.send_message(update.message.chat_id, 'Я занят общением с вами! Чем могу помочь?')
 
 def main():
-    persistence = PickleP
+    persistence = PicklePersistence('/app/conversation_data.pkl')
+    updater = Updater(TOKEN, use_context=True, persistence=persistence)
+
+    # Добавленные строки
+    PORT = int(os.environ.get('PORT', 5000))
+    updater.start_polling(port=PORT)
+
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help_command))
+
+    dp.add_handler(MessageHandler(Filters.regex(r'как дела\??') | Filters.regex(r'как дила\??'), how_are_you))
+    dp.add_handler(MessageHandler(Filters.regex(r'что ты делаешь\??'), what_are_you_doing))
+
+    logging.info("Bot started")
+
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
